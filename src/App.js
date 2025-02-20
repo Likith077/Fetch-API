9fb740d7a3beecfb06c677204f86deb9955d8b8a
Likith Nirvan <152361721+Likith077@users.noreply.github.com>
2025-02-20 12:51:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Users List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersList;
